72aa4ddc90261c9b53e02207c555f86c8ad31463
rmuralijr1 <murali.ramaneegan@gmail.com>
2025-07-20 00:30:34 +0100

    Add files via upload
    week1 day1

---
+++ day1/HelloTestLeaf.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class HelloTestLeaf {
+	public static void main(String[] args) {
+		String a = "hello world"; 
+		System.out.println("a");
+		
+		int d= 5;
+		int b= 6;
+		System.out.println(d+b);
+
+		
+			
+	}
+
+}
---
+++ day1/chrome.java
@@ -0,0 +1,26 @@
+package week1.day1;
+
+public class chrome {
+
+	public static void main(String[] args) {
+		float version= 91.0f;
+		String developer = "google";
+		boolean beta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+		
+		System.out.println(version);
+		System.out.println(developer);
+		System.out.println(beta);
+		System.out.println(releaseYear);
+		System.out.println(shortcutKey);
+		System.out.println("verison"+"="+version);
+		System.out.println("developer"+"="+developer);
+		System.out.println("beta"+"="+beta);
+		System.out.println("shortcutkey"+"="+shortcutKey);	
+		System.out.println("releaseYear"+"="+releaseYear);
+		System.out.println("releaseYear"+"="+releaseYear+"("+version+")" );
+		
+	}
+
+}
